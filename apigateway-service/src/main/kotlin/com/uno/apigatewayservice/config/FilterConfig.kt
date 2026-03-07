1068552bafcbee20a62d75ae2bb6853506487845
JeonNewJin <egjeon8@gmail.com>
2026-03-07 23:25:06 +0900

    feat: Spring Cloud Gateway - Filter 적용

---
+++ apigateway-service/src/main/kotlin/com/uno/apigatewayservice/config/FilterConfig.kt
@@ -0,0 +1,31 @@
+package com.uno.apigatewayservice.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+
+//    @Bean
+    fun routes(builder: RouteLocatorBuilder): RouteLocator =
+        builder.routes()
+            .route("first-service") { r ->
+                r.path("/first-service/**")
+                    .filters { f ->
+                        f.addRequestHeader("first-request", "1st-request-header")
+                         .addResponseHeader("first-response", "1st-response-header")
+                    }
+                    .uri("http://localhost:8081")
+            }
+            .route("second-service") { r ->
+                r.path("/second-service/**")
+                    .filters { f ->
+                        f.addRequestHeader("second-request", "2nd-request-header")
+                         .addResponseHeader("second-response", "2nd-response-header")
+                    }
+                    .uri("http://localhost:8082")
+            }
+            .build()
+}
